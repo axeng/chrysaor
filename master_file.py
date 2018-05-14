21241727d3158b69b84a182e37426f1bef3ff3d7
Axen Georget <mail@axen.io>
2018-05-13 22:05:36 -0700

    add a test loop

--- master_file.py
+++ master_file.py
@@ -1,20 +1,24 @@
 import machine_learning.analysis as a
 import twitter_api.top_crypto_tweets as ml
 
-test = ml.returnBtcTweets()
 
-# print(a.eval(test[0]))
+while True:
+    test = ml.returnBtcTweets()
 
-aftermath = []
-for i in test :
-    aftermath.append(a.eval(i))
+    # print(a.eval(test[0]))
 
-positive = 0
-negative = 0
+    aftermath = []
+    for i in test :
+        aftermath.append(a.eval(i))
 
-for r in aftermath :
-    if r > 0 :
-        positive += 1
-    else :
-        negative += 1
-print (positive, negative)
+    positive = 0
+    negative = 0
+
+    for r in aftermath :
+        if r > 0 :
+            positive += 1
+        else :
+            negative += 1
+    print (positive, negative)
+
+    wait = input("")
